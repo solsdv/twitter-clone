0f5cd419ed37891e7512ebcdcc72f2c5b006d873
Steven <murkappco@gmail.com>
2021-05-12 18:15:56 -0400

    add db

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(
+  process.env.MONGO_ATLAS_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  () => {
+    console.log("Successfully connected to DB!");
+  }
+);
 
 
 app.get("/", (req, res) => {
     res.send('<h1>Hello Express!</h1>');
 })
-
-app.get("/hello", (req, res) => {
-    res.send('<h1>Hello!</h1>');
-});
-
 
 app.get("/ping", (req, res) => {
     res.json({inset: "haldljksaf"})
