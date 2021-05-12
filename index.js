58edafdef5d01693bfbb1dfc63e659944fa6b567
Steven <murkappco@gmail.com>
2021-05-12 18:26:23 -0400

    add port support

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get("/ping", (req, res) => {
 });
 
 
-app.listen(5000, (req, res) => {
+app.listen((process.env.PORT || 5000), (req, res) => {
     console.log("server started on port 5000");
 })
